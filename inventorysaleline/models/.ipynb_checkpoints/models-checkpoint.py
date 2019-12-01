0e038d46bf682f2c376fcd20108c52b4043d3869
abudulalibayo <abudlalibayo@gmail.com>
2019-12-01 10:08:00 +0000

    yes 2

--- inventorysaleline/models/.ipynb_checkpoints/models-checkpoint.py
+++ inventorysaleline/models/.ipynb_checkpoints/models-checkpoint.py
@@ -7,8 +7,8 @@ from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
-
-    warehouse_id = fields.Many2one(
+    
+    'warehouse_id' = fields.Many2one(
         'stock.warehouse', string='Warehouse',
         required=False, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
         default=None, check_company=True)
