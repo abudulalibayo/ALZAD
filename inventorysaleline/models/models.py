3c4d1530ec8d542a26d9cb665f0997102a0aab27
libtech <abudlalibayo@gmail.com>
2019-12-30 15:03:51 -0500

    Update models.py

--- inventorysaleline/models/models.py
+++ inventorysaleline/models/models.py
@@ -4,8 +4,8 @@
 from odoo import models, fields, api
 
 
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
+class SaleOrderlinewarehouse(models.Model):
+    _inherit = 'sale.order'
     
     warehouse_id = fields.Many2one(
         'stock.warehouse', 
